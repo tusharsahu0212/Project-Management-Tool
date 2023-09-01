df70b5e69ca9b1db8bb8b7f456a57f02e2eb7928
tusharsahu0212 <tusharsahu0212@gmail.com>
2023-09-01 12:16:04 +0530

    complete task

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+
+
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
+
 
 main().catch(err => console.log(err));
 
@@ -70,8 +73,8 @@ const Task = new mongoose.model('Task', taskSchema);
 const User = new mongoose.model('User', userSchema);
 
 
-
-app.get('/', (req,res)=>{
+//Home
+app.get('/', (req, res) => {
 
     res.render('Home');
 });
@@ -79,66 +82,67 @@ app.get('/', (req,res)=>{
 
 // User
 
+
 //signUp
 
-app.get('/signUp', (req,res)=>{
+app.get('/signUp', (req, res) => {
 
     res.render('signUp');
 });
 
-app.post('/signUp', (req,res)=>{
+app.post('/signUp', (req, res) => {
 
-    User.findOne({userId: req.body.userId}).then((data,err)=>{
+    User.findOne({ userId: req.body.userId }).then((data, err) => {
 
-        if(data){
-           return res.render('login')
-        }else{
+        if (data) {
+            return res.render('login')
+        } else {
             const newUser = new User({
 
                 userId: req.body.userId,
                 userPassword: req.body.userPassword,
                 Name: req.body.userName
-        
+
             });
-        
+
             newUser.save();
-        
+
             userIdSession = req.body.userId;
-            userPasswordSession = req.body.userPassword;    
-        
-            res.render('JoinedProjects',{myProjects: []})
+            userPasswordSession = req.body.userPassword;
+
+            res.render('JoinedProjects', { myProjects: [] })
         }
     });
-    
+
 });
 
 
 
 
 //login
-app.post('/login', (req,res)=>{
+app.post('/login', (req, res) => {
 
-    User.findOne({userId:req.body.userId, userPassword:req.body.userPassword}).then((data,err)=>{
+    User.findOne({ userId: req.body.userId, userPassword: req.body.userPassword }).then((data, err) => {
 
-        if(data){
+        if (data) {
             userIdSession = req.body.userId;
-            userPasswordSession = req.body.userPassword;  
+            userPasswordSession = req.body.userPassword;
 
-            res.render('JoinedProjects',{myProjects: data.joinedProjects})
-        }else{
+            res.render('JoinedProjects', { myProjects: data.joinedProjects })
+        } else {
             res.redirect('/signup');
         }
     });
 });
 
-app.get('/login', (req,res)=>{
+app.get('/login', (req, res) => {
 
     res.render('login');
 });
 
 // logout
 
-app.get('/logout', (req,res)=>{
+app.get('/logout', (req, res) => {
 
     userIdSession = null;
     userPasswordSession = null;
@@ -147,17 +151,18 @@ app.get('/logout', (req,res)=>{
 
 //projects
 
-app.get('/allProjects', (req,res)=>{
+app.get('/allProjects', (req, res) => {
 
 
-    Project.find().then((data,err)=>{
+    Project.find().then((data, err) => {
 
-        if(!err){
-            if(userIdSession){
+        if (!err) {
 
-                res.render('AllProjects',{allProjects: data,user: userIdSession});
-            }else{
-                res.render('AllProjects',{allProjects: data,user: null});
+            if (userIdSession) {
+
+                res.render('AllProjects', { allProjects: data, user: userIdSession });
+            } else {
+                res.render('AllProjects', { allProjects: data, user: null });
 
             }
         }
@@ -165,37 +170,37 @@ app.get('/allProjects', (req,res)=>{
     });
 });
 
-app.get('/joinedProjects', (req,res)=>{
+app.get('/joinedProjects', (req, res) => {
 
-    if(userIdSession && userPasswordSession){
-        User.findOne({userId: userIdSession}).then((data,err)=>{
+    if (userIdSession && userPasswordSession) {
+        User.findOne({ userId: userIdSession }).then((data, err) => {
 
-            if(!err){
-              return res.render('JoinedProjects',{myProjects: data.joinedProjects})
+            if (!err) {
+                return res.render('JoinedProjects', { myProjects: data.joinedProjects })
             }
-    
+
             console.log(err);
         });
-    }else{
+    } else {
 
         return res.redirect('/login');
     }
 
 });
 
-app.get('/joinTheProject', (req,res)=>{
+app.get('/joinTheProject', (req, res) => {
 
-    if(userIdSession && userPasswordSession){
-        User.findOne({userId: userIdSession}).then((user,err)=>{
+    if (userIdSession && userPasswordSession) {
+        User.findOne({ userId: userIdSession }).then((user, err) => {
 
-            if(!err){
+            if (!err) {
 
-                Project.findOne({_id:req.query.projectId}).then((project,err)=>{
+                Project.findOne({ _id: req.query.projectId }).then((project, err) => {
                     let previousMembers = project.projectMembers;
                     previousMembers.push(userIdSession);
 
-                    Project.updateOne({_id:req.query.projectId},{projectMembers:previousMembers}).then((project,err)=>{
-                        if(err){
+                    Project.updateOne({ _id: req.query.projectId }, { projectMembers: previousMembers }).then((project, err) => {
+                        if (err) {
                             console.log(err);
                         }
                     });
@@ -203,124 +208,98 @@ app.get('/joinTheProject', (req,res)=>{
                     let previousProjects = user.joinedProjects;
                     // console.log("pre",previousProjects);
                     previousProjects.push(project);
-        
-                    User.updateOne({ userId: userIdSession}, { joinedProjects: previousProjects }).then((data, err) => {
-        
+
+                    User.updateOne({ userId: userIdSession }, { joinedProjects: previousProjects }).then((data, err) => {
+
                         if (err) {
-                            console.log("err",err);
-                        }else{
+                            console.log("err", err);
+                        } else {
                             // console.log("data",user);
                         }
-                
+
                     });
                 });
 
                 res.redirect('joinedProjects');
             }
-    
+
             console.log(err);
         });
-    }else{
+    } else {
 
         return res.redirect('/login');
     }
 
 });
 
-app.get('/projectRoom', (req,res)=>{
+app.get('/projectRoom', (req, res) => {
 
-    Project.findOne({_id:req.query.projectId}).then((project,err)=>{
+    Project.findOne({ _id: req.query.projectId }).then((project, err) => {
 
-        if(!err){
+        if (!err) {
 
-            res.render('projectRoom',{projectData:project});
+            res.render('projectRoom', { projectData: project, user: userIdSession });
 
-        }else{
+        } else {
             console.log(err);
         }
     });
 });
 
-app.get('/projectInfo', (req,res)=>{
+app.get('/projectInfo', (req, res) => {
 
-    Project.findOne({_id:req.query.projectId}).then((project,err)=>{
+    Project.findOne({ _id: req.query.projectId }).then((project, err) => {
 
-        if(!err){
+        if (!err) {
 
-            res.render('projectInfo',{projectData:project});
+            res.render('projectInfo', { projectData: project });
 
-        }else{
+        } else {
             console.log(err);
         }
     });
 });
 
-app.get('/projectMembers', (req,res)=>{
+app.get('/projectMembers', (req, res) => {
 
-    Project.findOne({_id:req.query.projectId}).then((project,err)=>{
+    Project.findOne({ _id: req.query.projectId }).then((project, err) => {
 
-        if(!err){
+        if (!err) {
 
-            res.render('projectMembers',{projectData:project});
+            res.render('projectMembers', { projectData: project, user: userIdSession });
 
-        }else{
+        } else {
             console.log(err);
         }
     });
 });
 
-app.get('/projectTasks', (req,res)=>{
+app.get('/projectTasks', (req, res) => {
 
-    Project.findOne({_id:req.query.projectId}).then((project,err)=>{
 
-        if(!err){
+    Project.findOne({ _id: req.query.projectId }).then((project, err) => {
 
-            res.render('projectTasks',{projectTasks:project.projectTasks});
+        if (project) {
 
-        }else{
+            res.render('projectTasks', { projectData: project });
+
+        } else {
             console.log(err);
         }
     });
 });
 
-app.get('/tasksAssignedByYou', (req,res)=>{
 
-    Project.findOne({_id:req.query.projectId}).then((project,err)=>{
+app.get('/createProject', (req, res) => {
 
-        if(!err){
-
-            res.render('TasksAssignedByYou',{projectTasks:project.projectTasks,user: userIdSession});
-
-        }else{
-            console.log(err);
-        }
-    });
-});
-
-app.get('/tasksAssignedToYou', (req,res)=>{
-
-    Project.findOne({_id:req.query.projectId}).then((project,err)=>{
-
-        if(!err){
-
-            res.render('TasksAssignedToYou',{projectTasks:project.projectTasks,user: userIdSession});
-
-        }else{
-            console.log(err);
-        }
-    });
-});
-
-app.get('/createProject', (req,res)=>{
-
-    if(userIdSession==null && userPasswordSession==null){
+    if (userIdSession == null && userPasswordSession == null) {
         res.redirect('/login');
     }
 
     res.render('CreateProject');
 });
 
-app.post('/createProject', (req,res)=>{
+app.post('/createProject', (req, res) => {
 
 
 
@@ -338,11 +317,11 @@ app.post('/createProject', (req,res)=>{
     joinedNewProject = [newProject];
 
 
-    User.findOne({ userId: userIdSession, userPassword: userPasswordSession }).then((data,err)=>{
+    User.findOne({ userId: userIdSession, userPassword: userPasswordSession }).then((data, err) => {
 
         if (err) {
-            console.log("err",err);
-        }else{
+            console.log("err", err);
+        } else {
             // console.log("data",data);
             let previousProjects = data.joinedProjects;
             // console.log("pre",previousProjects);
@@ -351,16 +330,14 @@ app.post('/createProject', (req,res)=>{
             User.updateOne({ userId: userIdSession, userPassword: userPasswordSession }, { joinedProjects: previousProjects }).then((data, err) => {
 
                 if (err) {
-                    console.log("err",err);
-                }else{
+                    console.log("err", err);
+                } else {
                     // console.log("data",data);
                 }
-        
+
             });
 
         }
-
-
 
     });
 
@@ -370,8 +347,170 @@ app.post('/createProject', (req,res)=>{
 
 });
 
+//Task 
 
+app.get('/createTask', (req, res) => {
+    // console.log(req.query)
+
+    res.render('CreateTask', { taskToBeAssigned: req.query.memberId, projectName: req.query.projectName, projectId:req.query.projectId });
+});
+
+app.post('/assignTask', (req, res) => {
+
+    // console.log(req.body)
+    const newTask = new Task({
+
+        taskTitle: req.body.taskTitle,
+        taskDescription: req.body.taskDescription,
+        taskSolution: null,
+        assignedTo: req.body.taskToBeAssigned,
+        assignedBy: userIdSession,
+        project: req.body.projectName
+    });
+
+    newTask.save();
+
+    Project.findOne({ _id: req.body.projectId }).then((project, err) => {
+        if (!err) {
+            // console.log(project);
+
+            previousTasks = project.projectTasks;
+            previousTasks.push(newTask);
+
+            Project.updateOne({ _id: req.body.projectId }, { projectTasks: previousTasks }).then((data, err) => {
+
+                if (err) {
+                    console.log(err);
+                }
+            });
+        } else {
+            console.log(err);
+        }
+
+    });
+
+    // update taskPosted of task sender
+    User.findOne({ userId: userIdSession }).then((user, err) => {
+
+        if(!err){
+        previousTaskPosted = user.tasksPosted;
+        previousTaskPosted.push(newTask);
+
+        User.updateOne({ userId: userIdSession },{tasksPosted:previousTaskPosted}).then((user,err)=>{
+
+            if(err){
+                console.log(err);
+            }
+        });
+
+
+        }else{
+            console.log(err);
+        }
+    });
+
+    //update tasksAssigned to task receiver
+    User.findOne({ userId: req.body.taskToBeAssigned }).then((user, err) => {
+
+        if(!err){
+        previousTaskToBeAssigned = user.tasksAssigned;
+        previousTaskToBeAssigned.push(newTask);
+
+        User.updateOne({ userId: req.body.taskToBeAssigned },{tasksAssigned:previousTaskToBeAssigned}).then((user,err)=>{
+
+            if(err){
+                console.log(err);
+            }
+        });
+
+
+        }else{
+            console.log(err);
+        }
+    });
+
+    res.redirect(`/projectTasks?projectId=${req.body.projectId}`);
+
+});
+
+app.get('/tasksAssignedByYou', (req, res) => {
+
+    Project.findOne({ _id: req.query.projectId }).then((project, err) => {
+
+        if (!err) {
+
+            res.render('TasksAssignedByYou', { projectData: project, user: userIdSession });
+
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+app.get('/tasksAssignedToYou', (req, res) => {
+
+    Project.findOne({ _id: req.query.projectId }).then((project, err) => {
+
+        if (!err) {
+
+            res.render('TasksAssignedToYou', { projectData: project, user: userIdSession });
+
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+app.get('/taskRoom', (req,res)=>{
+
+
+    Project.findOne({_id: req.query.projectId}).then((project,err)=>{
+
+        if(!err){
+
+            Task.findOne({_id: req.query.taskId}).then((task,err)=>{
+    
+                if(!err){
+        
+                    if(req.query.submitFlag == 'true'){
+        
+                        res.render('TaskRoom',{task: task,projectData: project,submitFlag: true});
+                    }else{
+                        res.render('TaskRoom',{task: task,projectData: project,submitFlag: false});
+        
+                    }
+        
+                }else{
+                    console.log(err);
+                }
+            });
+        }else{
+            console.log(err);
+        }
+    });
+
+});
+
+app.post('/taskSolution', (req,res)=>{
+Project.findOne({_id: req.body.projectId}).then((data,err)=>{
+
+    if(!err){
+        Task.updateOne({_id: req.body.taskId},{taskSolution: req.body.taskSolution}).then((data,err)=>{
+
+            if(err){
+                console.log(err);
+            }else{
+                res.redirect(`taskRoom?taskId=${req.body.taskId}&projectId=${req.body.projectId}&submitFlag=${true}`);
+            }
+        });
+    }else{
+        console.log(err);
+
+    }
+});
+
+});
 
 app.listen(3000, () => {
     console.log("Server started on port 3000.");
-})
+});
